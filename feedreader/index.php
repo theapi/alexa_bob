8adfdfba8ab66983ce498b0ce4470819c1c591f8
Peter <petermclarke@gmail.com>
2017-03-08 10:01:35 +0000

    Start of feed reader

---
+++ feedreader/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$json = '
+{
+  "audioData": {
+    "title": "#338, An Interview with Jørgen Jakobsen",
+    "url" : "https://traffic.libsyn.com/theamphour/TheAmpHour-338-AnInterviewWithJorgenJakobsen.mp3"
+  }
+}
+';
+
+header('Content-Type: application/json');
+print $json;
