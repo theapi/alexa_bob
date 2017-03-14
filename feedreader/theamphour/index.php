5d7f541a9d21c95ce278772ff14d08807d71f050
theapi <petermclarke@gmail.com>
2017-03-14 21:29:56 +0000

    Latest podcast

--- feedreader/theamphour/index.php
+++ feedreader/theamphour/index.php
@@ -1,8 +1,13 @@
 <?php
 
+// http://theamphour.com/feed/podcast/
 $json = '
 {
     "audioData": [
+        {
+            "title": "#339, Look at nature and meet nerds",
+            "url" : "https://traffic.libsyn.com/theamphour/TheAmpHour-339-LookAtNatureAndMeetNerds.mp3"
+        },
         {
             "title": "#338, An Interview with Jørgen Jakobsen",
             "url" : "https://traffic.libsyn.com/theamphour/TheAmpHour-338-AnInterviewWithJorgenJakobsen.mp3"
