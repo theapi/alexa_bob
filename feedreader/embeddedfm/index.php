9be30477576f1fedc1c4199c5719d9949b26de6b
Peter <petermclarke@gmail.com>
2017-03-22 21:20:03 +0000

    Parse RSS feed

--- feedreader/embeddedfm/index.php
+++ feedreader/embeddedfm/index.php
@@ -1,25 +1,27 @@
 <?php
+require '../FeedReader.php';
 
-// Hard coded for now
 // http://embedded.fm/episodes?format=JSON
-$json = '
-{
-    "audioData": [
-        {
-            "title": "191: WHAT, YOGURT!?!",
-            "url" : "https://traffic.libsyn.com/makingembeddedsystems/embedded-ep191.mp3"
-        },
-        {
-            "title": "190: Trust Me, I’m Right",
-            "url" : "https://traffic.libsyn.com/makingembeddedsystems/embedded-ep190.mp3"
-        },
-        {
-            "title": "189: The Squishiness Factor",
-            "url" : "https://traffic.libsyn.com/makingembeddedsystems/embedded-ep189.mp3"
-        }
-    ]
-}
-';
+// $json = '
+// {
+//     "audioData": [
+//         {
+//             "title": "191: WHAT, YOGURT!?!",
+//             "url" : "https://traffic.libsyn.com/makingembeddedsystems/embedded-ep191.mp3"
+//         },
+//         {
+//             "title": "190: Trust Me, I’m Right",
+//             "url" : "https://traffic.libsyn.com/makingembeddedsystems/embedded-ep190.mp3"
+//         },
+//         {
+//             "title": "189: The Squishiness Factor",
+//             "url" : "https://traffic.libsyn.com/makingembeddedsystems/embedded-ep189.mp3"
+//         }
+//     ]
+// }
+// ';
+
+$reader = new FeedReader('http://embedded.fm/?format=rss');
 
 header('Content-Type: application/json');
-print $json;
+print $reader->getAudioData();
